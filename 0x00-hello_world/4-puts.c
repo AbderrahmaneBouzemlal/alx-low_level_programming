a23a5623422562a58e4c74973dfb6381448da04d
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-07 02:54:31 -0700

    added a file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-                   /* declared the main function*/
-int main(void) /* declared the main function*/
- /* declared the main function*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	/* we used the function puts to print the sentence
-	 * and \n for the new line
-	 */
-	puts("\"Programming is like building a multilingual puzzle\n");
-	        /* we used the function puts to print the sentence
-         * and \n for the new line
-         */
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
