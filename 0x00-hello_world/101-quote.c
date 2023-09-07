c8bce696663178d60ab5a845f04c4df982aa564e
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-07 04:40:41 -0700

    Added the last file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 
-int main(void) 
+/**
+ * main - write to stdout
+ * Return: Always 0
+ */
+int main(void)
 {
-	/* 
-         * Use the write system call to print the message to stderr (file descriptor 2)
-         */
-         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-         return (1); 
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
