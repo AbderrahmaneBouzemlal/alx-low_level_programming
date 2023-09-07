37372ae8adcfbd5b0062f3e07e96d24f96d82646
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-07 03:04:14 -0700

    added a file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	/**
+	 * return: Always 0 (Success)
+	 */
+	return (0);
 }
