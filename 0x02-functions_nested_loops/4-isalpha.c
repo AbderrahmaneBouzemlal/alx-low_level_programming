d5bf5d4241ba2649c954f0f3f7cefe15e8b472bf
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-12 02:26:48 -0700

    added a file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - checks for alphabitic character
  * @c: checks if the character is an alphabitic character
- * Return: 1 if the c is letter otherwise 0 
+ * Return: 1 if the c is letter otherwise 0
  */
 int _isalpha(int c)
 {
