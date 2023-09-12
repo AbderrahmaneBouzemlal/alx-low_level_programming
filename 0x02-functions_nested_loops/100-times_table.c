919bee51f74aaf141982b42f51ecec74be94c39d
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-12 08:48:37 -0700

    added a file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,32 +11,32 @@ void print_times_table(int n)
 	int j;
 	int k;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				k = i * j;
-                        	if (j != 0 && k < 10)
-                        	{
-                                	printf("  %d", k);
-                        	}
-				else if(k > 9 && k < 100)
+				if (j != 0 && k < 10)
+				{
+					printf("  %d", k);
+				}
+				else if (k > 9 && k < 100)
 				{
 					printf(" %d", k);
 				}
 				else
-                        	{
-                                	printf("%d", k);
-                        	}
-                        	if (j != n)
-                        	{
-                                	putchar(',');
-                                	putchar(' ');
-                        	}
-                	}
-                	putchar('\n');
+				{
+					printf("%d", k);
+				}
+				if (j != n)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			putchar('\n');
 		}
 	}
 }
