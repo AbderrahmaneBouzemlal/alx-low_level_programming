e9fe34bef65feb0a9c477134819c3ff5942c039b
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-12 12:11:22 -0700

    added a file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point prints the first 50 fibonacci numbers
+ * Return: Always 0 (Successeed)
+ */
+int main(void)
+{
+	int i;
+	int fibon;
+	int fibonaci[50];
+
+	fibonaci[0] = 1;
+	fibonaci[1] = 2;
+
+	for (i = 2; i <= 50; i++)
+	{
+		fibonaci[i] = fibonaci[i - 1] + fibonaci[i - 2];
+	}
+
+	fibon = 0;
+
+	while (fibon < 50)
+	{
+		printf("%d, ", fibonaci[fibon]);
+		fibon++;
+	}
+	printf("%d\n", fibonaci[50]);
+
+	return (0);
+}
