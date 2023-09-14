aaa116f17ee3ca8be594e4352b635499a15e026d
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-14 02:43:52 -0700

    added a file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isupper - checks for uppercase character
- * @c: the input for this function is alphabitical character 
+ * @c: the input for this function is alphabitical character
  * Return: when the input is uppercase 1 and 0 otherwise
  */
 int _isupper(int c)
