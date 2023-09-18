868ed18790df4427ec23d92413c8a44b4d1340ea
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-18 07:56:23 -0700

    added a file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,10 +16,10 @@ void rev_string(char *s)
 		len++;
 	}
 
-	for (i = 0; i < len/2; i++)
+	for (i = 0; i < len / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[len-i-1];
-		s[len-i-1] = temp;
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
