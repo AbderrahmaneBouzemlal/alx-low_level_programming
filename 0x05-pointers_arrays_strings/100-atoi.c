5130ee6c435f96c0e816b209bdeedb122443e0d6
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-18 22:23:11 -0700

    Added a description for the _atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * atoi - convert a string to an integer
+ * _atoi - convert a string to an integer
  * @s: The string to be converted
  * Return: the integer that converted from string
  */
