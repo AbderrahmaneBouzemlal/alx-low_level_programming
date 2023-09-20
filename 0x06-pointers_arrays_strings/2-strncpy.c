a27eb0f2075b6631075c882e81736ee60952f50c
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-20 01:53:56 -0700

    added a file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: A Pointer to a string that contain the copy of the source
+ * @src: A Pointer to a string that will be copied
+ * @n: The number of bytes to be copied
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
