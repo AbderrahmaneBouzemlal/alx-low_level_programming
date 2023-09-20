fc3c02c8a140a4cf8188142d80a8a3bd2adb8463
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-20 08:26:13 -0700

    added a file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,8 +8,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int s;
+	int sum = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-	}	
+		s = s1[i] - s2[i];
+		sum += s;
+	}
+	return (sum);
 }
