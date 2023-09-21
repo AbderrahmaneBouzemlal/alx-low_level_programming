5280201d2b50349d23fdc6559e9a39f756334a1e
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-21 10:18:44 -0700

    added a file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,54 +4,47 @@
 /**
  * base10 - power in 10 base
  * @n: an exponent
- * Return: returns 10 to power exponent
+ * Return: returns 10 to the power of exponent
  */
 int base10(int n)
 {
-	int base = 10;
+    int base = 10;
 
-	while (n > 0)
-	{
-		base *= 10;
-		n--;
-	}
-	return (base);
+    while (n > 0)
+    {
+        base *= 10;
+        n--;
+    }
+    return (base);
 }
 
 /**
- * print_number - prints integers enters as parameters using putchar
+ * print_number - prints integers entered as parameters using putchar
  * @n: integer to print
  * Return: void
  */
 void print_number(int n)
 {
-	int power;
+    int power;
 
-	power = base10(8);
+    power = base10(8);
 
-	if (n < 0)
-	{
-		putchar('-');
-		n *= -1;
-	}
+    if (n < 0)
+    {
+        putchar('-');
+        n *= -1;
+    }
 
-	if (n == 0)
-	{
-		putchar('0');
-	}
+    if (n == 0)
+    {
+        putchar('0');
+    }
 
-	else
-	{
-		while (n / power == 0)
-		{
-			power /= 10;
-		}
-
-		while (power >= 1)
-		{
-			putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
-		}
-	}
+    while (power > 0)
+    {
+        int digit = n / power;
+	putchar(digit + '0');
+	n = n % power;
+	power /= 10;
+    }
 }
