ed390b53aa1a6c6734acf4c4747d1dc6c7513a76
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-25 08:34:03 -0700

    added a file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * _memcpy - copies memory area
- * dest: a pointer which point to the destination of copying the memory
- * src: A pointer which point to the source of the memory
+ * @dest: a pointer which point to the destination of copying the memory
+ * @src: A pointer which point to the source of the memory
+ * @n: The number of memory bytes to be copied
  * Return: A pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
