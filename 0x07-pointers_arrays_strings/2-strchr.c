604bc96352c79670efc25e755c5008e4818bfcee
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-09-25 09:16:22 -0700

    added a file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - function that locates a character in a string
+ * @s: The string
+ * @c: The character to be found
+ * Return: A pointer to the occurrence of the character or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
+}
