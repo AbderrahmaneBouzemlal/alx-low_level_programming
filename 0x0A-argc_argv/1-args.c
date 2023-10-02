23eb5f49d458dda24ec451c456b450d58408b17c
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-10-02 12:41:24 -0700

    added a file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints the number of arguments passed into it
+ * @argc: The count of the arguments
+ * @argv: The array that containes the Arguments
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argv[0])
+	{
+		printf("%d\n", argc - 1);
+	}
+}
