f92a29abcd8923d75f66fabe0beb3654f19c8013
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-10-02 12:27:53 -0700

    added a file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of the file
+ * Return: 0 when secceed
+ */
+int main (int argc, char * argv[])
+{ 
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+In this project we will explore new thing which is the command line argument in c language
