c3e6aeabd9941a72e16ce79c0e94694f29f14b4a
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-10-02 12:31:40 -0700

    added a file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main - Prints the name of the file
+ * @argc: The number (count) of the arguments
+ * @argv: The Array of the Arguments
  * Return: 0 when secceed
  */
-int main (int argc, char * argv[])
-{ 
+int main(int argc, char *argv[])
+{
 	printf("%s\n", argv[argc - argc]);
 	return (0);
 }
