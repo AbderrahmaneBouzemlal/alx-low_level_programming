4f2c97971553c3a46c62a8208a7a13b0b5512c45
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2023-10-03 12:36:25 -0700

    added a file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	int i;
 
 	if (s1 == NULL)
-		return (NULL);
+		s1 = "";
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 	while (s1[len1] != '\0')
 	{
 		len1++;
@@ -26,10 +26,10 @@ char *str_concat(char *s1, char *s2)
 	{
 		len2++;
 	}
-	str = malloc((len1 + len2) * sizeof(char));
+	str = malloc((len1 + len2 + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < len1 + len2; i++)
+	for (i = 0; i <= len1 + len2; i++)
 	{
 		if (i < len1)
 		{
