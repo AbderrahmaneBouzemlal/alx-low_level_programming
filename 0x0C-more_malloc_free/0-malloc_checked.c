2ae31b5107e1fa5d2bb4b55c7758b962f86ae9d9
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2023-10-07 14:08:47 +0800

    Added a file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,13 +6,13 @@
  * @b: unsigned integer that refers to the number of the bytes
  * Return: a pointer to the allocated memory
  */
+void *malloc_checked(unsigned int b);
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
-	
+
 	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
-	
 	return (ptr);
 }
