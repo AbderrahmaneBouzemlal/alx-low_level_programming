47dd13ac1bdf010fbbc22d7d8b2c7e4a7d203148
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2023-10-08 19:07:08 +0800

    Added a file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,74 +1,41 @@
-#include "main.h"
 #include <stdlib.h>
-void *copy_allocate(void *ptr, unsigned int old_size, unsigned int new_size);
+#include "main.h"
 /**
- * copy_allocate - reallocates a memory block
- * @ptr: Is a pointer to the memory previously allocated
- * @old_size: Is the size of the allocated space for ptr
- * @new_size: Is the new size of the new memory block
- * Return: A pointer to the new allocated memory block
- */
-void *copy_allocate(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	unsigned int i;
-	void *rptr;
-	char *byte_ptr;
-	char *byte_rptr;
-
-	rptr = malloc(new_size);
-	if (rptr == NULL)
-	{
-		return (NULL);
-	}
-
-	byte_ptr = ptr;
-	byte_rptr = rptr;
-
-	for (i = 0; i < old_size && i < new_size; i++)
-	{
-		byte_rptr[i] = byte_ptr[i];
-	}
-	return (rptr);
-}
-
-/**
- * _realloc - Reallocates a memory block
- * @ptr: Is a pointer to the memory previously allocated
- * @old_size: Is the size of the allocated space for ptr
- * @new_size: Is the new size of the new memory block
- * Return: A pointer to the new allocated memory block
- */
-
+  * _realloc - reallocates a memory block using malloc and free.
+  * @ptr: pointer to the memory previously allocated with a
+  * call to ``` malloc : malloc(old_size) ```.
+  * @old_size: size in bytes allocated for ptr.
+  * @new_size: size in bytes of new memory block.
+  *
+  * Return: pointer to new mem block, NULL or ptr.
+  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *byte_ptr;
-
-	if (new_size == 0)
-	{
-		free(ptr);
-		byte_ptr = malloc(new_size);
-		return (byte_ptr);
-	}
-
-	if (ptr == NULL)
-	{
-		byte_ptr = malloc(new_size);
-		if (byte_ptr == NULL)
-			return (NULL);
-		return (byte_ptr);
-	}
+	char *nptr;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
+	if ((new_size == 0) && (ptr != NULL))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (NULL);
+	}
+	if (new_size > old_size && (ptr != NULL))
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (nptr);
+		for (i = 0; i < old_size; i++)
+			nptr[i] = *((char *)ptr + 1);
+		free(ptr);
+	}
 
-	if (old_size == 0)
-	{
-		ptr = malloc(new_size);
-		return (ptr);
-	}
-	if (new_size != old_size && ptr != NULL)
-	{
-		ptr = copy_allocate(ptr, old_size, new_size);
-	}
-	return (ptr);
+	return (nptr);
 }
