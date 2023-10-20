4b68991f91ddfafc65dce7cf556bde1511a6e2aa
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2023-10-20 08:29:11 +0800

    Added a file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_list - prints all the elments of a linked list
+ * @h: a singly linked list to be printed
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num;
+
+	num = 0;
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		num++;
+	}
+	return (num);
+
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Now we are going to explore the first data structure which is the singly linked list
