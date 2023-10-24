ca8e860f2e9c5a8b13833530cd99a2e4ff2ea69d
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2023-10-24 14:28:57 +0800

    Added a file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: The address of the First node in the list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = *head;
+
+	while(ptr)
+	{
+		listint_t *next = ptr->next;
+
+		free(ptr);
+		ptr = next;
+	}
+	*head = ptr;
+}
