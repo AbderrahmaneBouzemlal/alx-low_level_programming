6c2a35000dccafaf1c58bab5d38c6ab42a27df65
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2023-10-24 20:00:15 +0800

    Added a file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,9 +22,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node->n = n;
 	node->next = NULL;
 
-	if (idx == 0)
+	if (*head == NULL && idx == 0)
 	{
-		(*head)->next = node;
+		*head = node;
 		return (node);
 	}
 	if (*head == NULL || head == NULL)
