9a875e0a2b54e98910808601896b3b28a2f574db
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2023-10-26 09:57:31 +0800

    Added a file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,15 +9,23 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 0;
+	long int d;
 
 	if (head == NULL)
 		exit(98);
 
 	while (head)
 	{
-		printf("%d\n", head);
-		head = head->next;
+		d = head - head->next;
 		i++;
+		printf("[%p] %d\n", (void *)head, (head)->n);
+		if (d > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *) head->next, head->next->n);
+			break;
+		}
 	}
 	return (i);
 }
