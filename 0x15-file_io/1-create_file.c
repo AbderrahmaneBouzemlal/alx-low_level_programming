ee40e4bc92f8aebef804f666d9a5b2210fac8894
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2023-10-31 01:36:13 +0800

    Added a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,6 +23,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+		bytes_written = write(fd, "", 0);
 
 	bytes_written = write(fd, text_content, strlen(text_content));
 
