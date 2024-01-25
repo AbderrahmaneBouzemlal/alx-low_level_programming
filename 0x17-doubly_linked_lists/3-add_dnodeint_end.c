9ebdbcc630156863028cea3b8901e1bfb9012fd8
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-25 21:30:39 +0800

    Added a file

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,6 +9,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
 	dlistint_t *ptr = *head;
+
 	if (head == NULL)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
