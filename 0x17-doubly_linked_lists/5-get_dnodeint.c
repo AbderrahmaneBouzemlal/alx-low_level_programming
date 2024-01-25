23930a340f3339541004866915dd5f07c75bb3ea
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-25 22:44:28 +0800

    Added a file

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function that find the nth node
+ * @head: the first node in a list
+ * @index: index of node
+ * Return: the address of the node or NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr = NULL;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	ptr = head;
+	while (ptr->next)
+	{
+		if (count == index)
+			return (ptr);
+		ptr = ptr->next;
+		count++;
+	}
+	return (NULL);
+}
