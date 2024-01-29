66571f24d96a7ddbf29c2cc7927cbece0b4fcb60
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-29 18:17:37 +0800

    Added a file

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+in this directory we are going to delve more in c language by exploring the dynamic libraries
---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c)
+{
+	printf("%c", c);
+	return (0);
+}
+int _islower(int c)
+{
+	printf("islower\n");
+	return (0);
+}
+int _isalpha(int c)
+{
+	printf("isalpha\n");
+	return (0);
+}
+int _abs(int n)
+{
+	printf("abc\n");
+	return (0);
+}
+int _isupper(int c)
+{
+	printf("isupper\n");
+	return (0);
+}
+int _isdigit(int c)
+{
+	printf("isdigit\n");
+	return (0);
+}
+int _strlen(char *s)
+{
+	printf("strlen\n");
+	return (0);
+}
+void _puts(char *s)
+{
+	printf("%s", s);
+}
+char *_strcpy(char *dest, char *src)
+{
+	return (dest);
+}
+int _atoi(char *s)
+{
+	int number;
+
+	number = atoi(s);
+	return (number);
+}
+char *_strcat(char *dest, char *src)
+{
+	return (dest);
+}
+char *_strncat(char *dest, char *src, int n)
+{
+	return (dest);
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (dest);
+}
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (dest);
+}
+char *_strchr(char *s, char c)
+{
+	return (s);
+}
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+char *_strpbrk(char *s, char *accept)
+{
+	return (s);
+}
+char *_strstr(char *haystack, char *needle)
+{
+	return (needle);
+}
