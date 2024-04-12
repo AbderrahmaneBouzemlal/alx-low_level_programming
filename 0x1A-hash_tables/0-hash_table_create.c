b066bce042d317095199c4740214fd2204f90e95
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-13 07:35:32 +0800

    Added a file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,8 +15,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (hash_table->array == NULL)
+	if (!hash_table->array)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 	for (; i < size; i++)
 	{
 		hash_table->array[i] = NULL;
