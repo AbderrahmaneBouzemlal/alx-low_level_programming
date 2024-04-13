2e6f6bbdf6640b46c33054eeae67792fa516fc3c
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-13 08:17:27 +0800

    Added a file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,18 +16,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	printf("%lu\n", index);
-
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
-
 	node->key = strdup(key);
 	if (!node->key)
 	{
 		free(node);
 		return (0);
 	}
-
 	node->value = strdup(value);
 	if (!node->value)
 	{
@@ -35,18 +32,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node);
 		return (0);
 	}
-
 	node->next = NULL;
-
 	entry = ht->array[index];
 	if (!entry)
 	{
 		ht->array[index] = node;
 		return (1);
 	}
-
 	node->next = ht->array[index];
 	ht->array[index] = node;
-
 	return (1);
 }
