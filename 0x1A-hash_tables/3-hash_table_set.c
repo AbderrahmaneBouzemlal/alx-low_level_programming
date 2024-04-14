187c0d392c1c6273fa49e75d40947bb199574bc1
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-15 03:41:26 +0800

    Added a file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,14 +9,12 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-
 	hash_node_t *node;
 	hash_node_t *new_node;
 	unsigned long int index;
 
 	if (ht == NULL || *key == '\n' || *value == '\n')
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 
@@ -25,12 +23,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_node = create_new_node(key, value);
 		if (new_node == NULL)
 			return (0);
-
 		ht->array[index] = new_node;
 		return (1);
 	}
-
-/*If key exists, replace value*/
 	while (node != NULL)
 	{
 		if (strcmp(key, node->key) == 0)
@@ -41,7 +36,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		node = node->next;
 	}
-/*If key doesn't exist, create new node*/
 	new_node = create_new_node(key, value);
 	if (new_node == NULL)
 		return (0);
